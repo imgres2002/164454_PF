e7c8a56ad56e294c58e14e1fd0f40a5737ae083d
imgres <92025754+imgres2002@users.noreply.github.com>
2024-05-14 18:43:01 +0200

    Add lab12

---
+++ lab12/strona2.php
@@ -0,0 +1,14 @@
+<?php
+// Sprawdź, czy dane zostały przesłane
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sprawdź, czy login i hasło są poprawne
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+
+    if ($login == "Janusz" && $password == "1234") {
+        echo "Zalogowano pomyślnie!";
+    } else {
+        echo "Błąd logowania. Spróbuj ponownie.";
+    }
+}
+?>
